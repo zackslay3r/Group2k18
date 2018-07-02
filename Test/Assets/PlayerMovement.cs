3e175f8aaec2a68b3c0cb4367c255a2b16559437
Zackary D <zackary.direen@hotmail.com>
2018-07-02 15:55:05 +1000

    Inital repo
    Input was changed so that d is forward and a is backward

---
+++ Test/Assets/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿// this player controller is based off of 
+// http://www.instructables.com/id/Unity-25D-Character-Control-in-Platform-Side-scrol/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    //Animator anim;
+    public float smooth = 1f;
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Quaternion lookLeft;
+    private Quaternion lookRight;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        //anim = GetComponent();
+        Time.timeScale = 1;
+
+        lookRight = transform.rotation;
+        lookLeft = lookRight * Quaternion.Euler(0, 180, 0);
+    }
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+
+            //anim.SetBool("IsRunning", false);
+
+            //moveDirection = new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+            moveDirection = new Vector3(Input.GetAxis("Vertical"), 0,0);
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+            //if (Input.GetKey(KeyCode.A))
+            //{
+
+            //    transform.rotation = lookLeft;
+            //    moveDirection = transform.TransformDirection(-moveDirection);
+            //    moveDirection *= speed;
+
+            //   // anim.SetBool("IsRunning", true);
+
+            //}
+
+            //if (Input.GetKey(KeyCode.D))
+            //{
+            //    transform.rotation = lookRight;
+            //    moveDirection = transform.TransformDirection(moveDirection);
+            //    moveDirection *= speed;
+            //    //anim.SetBool("IsRunning", true);
+            //}
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * speed * Time.deltaTime);
+    }
+}
