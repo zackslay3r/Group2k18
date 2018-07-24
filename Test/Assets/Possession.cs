5d62c28042681ba7f6ccffd09d59d979d39ef1a1
Zackary D <zackary.direen@hotmail.com>
2018-07-24 15:30:26 +1000

    start of possession

---
+++ Test/Assets/Possession.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Possession : MonoBehaviour {
+
+    // This will contain the list of the controllable characters.
+
+    public GameObject[] possessables;
+
+    //This is the current possessible character;
+
+    GameObject currentCharacter;
+
+
+    public float distanceAway;
+
+    // This is the camera.
+    public CameraFol mainCamera;
+    
+	// Use this for initialization
+	void Start () {
+        currentCharacter = mainCamera.player; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            foreach (GameObject characters in possessables)
+            {
+                if (characters == currentCharacter)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (Vector3.Distance(currentCharacter.transform.position, characters.transform.position) < distanceAway)
+                    {
+                        currentCharacter = characters;
+                        mainCamera.player = characters;
+                    };
+                }
+            }
+        }
+
+	}
+}
